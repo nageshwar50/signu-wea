eca4a70b5dae08f15477ecd424b021628a6adb79
Nageshwar <nageshwarpandey198@gmail.com>
2023-12-03 20:35:35 +0530

    dockercompose file updated

--- db.js
+++ db.js
@@ -5,15 +5,14 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env'})
 
 const pool = mysql.createPool({
-    host: process.env.DB_host,
-    user: process.env.DB_user,
-    password: process.env.DB_password,
-    database: process.env.database,
+    host: '127.0.0.1',
+    user: 'nageshwar',
+    password: 'DtnPkAlpxWTC9CkE:',
+    database: 'NAGESHWAR',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
-
 // To get a connection from the pool:
 pool.getConnection((err, connection) => {
     if (err) {
